afa1ea7ad16958587b9d53938ebfe86e64200e5c
UzukiAme <aprilddev@gmail.com>
2015-05-01 05:47:12 -0400

    initial commit

--- profnav/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../includes/functions.php");
-include_once("../includes/header.php");
-?>
-
-    <script type="text/javascript" src="js/styling.js"></script>
-    <script type="text/javascript" src="js/animation.js"></script>
-  </body>
-</html>
